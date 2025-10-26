842ce6d73770ad3d3b12fce4248df577be71d768
JpMayor1 <royamsemaj@gmail.com>
2025-10-26 15:51:30 +0800

    Added billion format

--- frontend/src/utils/amount/formatAmount.ts
+++ frontend/src/utils/amount/formatAmount.ts
@@ -1,6 +1,9 @@
 export const formatAmount = (amount: number): string => {
-  if (amount >= 1_000_000) {
+  if (amount >= 1_000_000_000) {
+    return (amount / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B";
+  } else if (amount >= 1_000_000) {
     return (amount / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
+  } else {
+    return amount.toLocaleString();
   }
-  return amount.toLocaleString();
 };
